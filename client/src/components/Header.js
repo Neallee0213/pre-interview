b97cf4cef1760810716b47d6b54e30962d8a9fcc
Neal Lee <hyunjoon.lee0213@gmail.com>
2019-12-20 00:08:34 -0500

    first commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -50,7 +50,7 @@ const Head = styled.nav`
 const Header = () => {
     return (
         <Head className="navbar navbar-expand-lg navbar-light">
-            <span className="navbar-brand" href="#">Hacker News</span>
+            <span className="navbar-brand" href="#">HYUNJOON LEE</span>
             <button className="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <FaBars className="navbar-toggler-icon" />
             </button>
